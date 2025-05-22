c5ab319c1f4adbe72c7a8b8e9fdfef980f1bf92d
Cole Sexton <colesaudio88@gmail.com>
2025-05-22 16:15:12 -0500

    basic song form completed

--- coles-webpage/src/components/SongForm.jsx
+++ coles-webpage/src/components/SongForm.jsx
@@ -9,27 +9,39 @@ const [formData, setFormData] = useState({
     genre:"",
     style:"",
     instruments:[],
-    lyrics:"",
+    lyricsIncluded:"",
+    lyricsText:"",
     length:"",
     forSomeone:"",
+    forSomeoneExplain:"",
     emotions:"",
     extraInfo:"",
-    bounceType:""
+    bounce:[]
 
 });
+
+const bounceOptions = ["mp3", "wav", ".logix", "cd", "tape"];
+const formatLabels = {
+  "mp3": "MP3",
+  "wav": "WAV",
+  ".logix": "Logic File",
+  "cd": "CD",
+  "tape": "Cassette Tape"
+};
+
 
 
 const handleChange = (e) =>{
     const {name, value, type, checked} = e.target;
 
-    if(name === "instruments"){
+    if(name === "instruments" || name === "bounce"){
         setFormData((formData) =>{
             const updated = checked 
-            ? [...formData.instruments, value]
-            : formData.instruments.filter((instrument) => instrument !== value);
+            ? [...formData[name], value]
+            : formData[name].filter((item) => item !== value);
             return{
                 ...formData,
-                instruments: updated
+                [name]: updated
             };
 
         });
@@ -84,12 +96,16 @@ const handleChange = (e) =>{
             </label>
             <br />
 
-            <label for="style">Style:
+            <label for="style">Style: <br />
             <textarea type="text" id="style" name="style" rows="4" cols="40" maxLength="400" placeholder="Eleborate on more specific genres. Feel free to use standards like Classic Rock, or create your own and be descriptive " value={formData.style} onChange={handleChange}></textarea>
             </label>
             <br />
+            
+            <fieldset className="insBoxes">
 
-            <label for="instruments">Instruments:
+            <legend>Instruments </legend>
+            
+            <label>
             <input 
             type="checkbox" 
             name="instruments" 
@@ -99,9 +115,195 @@ const handleChange = (e) =>{
             Acousitc Guitar
             </label>
 
+            <label>
+            <input 
+            type="checkbox" 
+            name="instruments" 
+            value="electric"
+            checked={formData.instruments.includes("electric")} 
+            onChange={handleChange} />
+            Electric Guitar
+            </label>
+
+            <label>
+            <input 
+            type="checkbox" 
+            name="instruments" 
+            value="bass"
+            checked={formData.instruments.includes("bass")} 
+            onChange={handleChange} />
+            Bass
+            </label>
+
+            <label>
+            <input 
+            type="checkbox" 
+            name="instruments" 
+            value="mandolin"
+            checked={formData.instruments.includes("mandolin")} 
+            onChange={handleChange} />
+            Mandolin
+            </label>
+
+            <label>
+            <input 
+            type="checkbox" 
+            name="instruments" 
+            value="piano"
+            checked={formData.instruments.includes("piano")} 
+            onChange={handleChange} />
+            Piano
+            </label>
+            <br />
+
+            <label>
+            <input 
+            type="checkbox" 
+            name="instruments" 
+            value="keyboard"
+            checked={formData.instruments.includes("keyboard")} 
+            onChange={handleChange} />
+            Electric Keyboard
+            </label>
+
+            <label>
+            <input 
+            type="checkbox" 
+            name="instruments" 
+            value="synth"
+            checked={formData.instruments.includes("synth")} 
+            onChange={handleChange} />
+            Synthesizer
+            </label>
+            
+
+            <label>
+            <input 
+            type="checkbox" 
+            name="instruments" 
+            value="drums"
+            checked={formData.instruments.includes("drums")} 
+            onChange={handleChange} />
+            Drum Kit
+            </label>
+
+            <label>
+            <input 
+            type="checkbox" 
+            name="instruments" 
+            value="percussion"
+            checked={formData.instruments.includes("percussion")} 
+            onChange={handleChange} />
+            Percussion
+            </label>
+
+            <label>
+            <input 
+            type="checkbox" 
+            name="instruments" 
+            value="vocal"
+            checked={formData.instruments.includes("vocal")} 
+            onChange={handleChange} />
+            Vocal
+            </label>
+            </fieldset>
+
+            <fieldset>
+            <legend>Lyrics</legend>
+                <label for="lyricsYes">
+                <input type="radio" id="lyricsYes" name="lyricsIncluded" value="yes" checked={formData.lyricsIncluded === "yes"} onChange={handleChange}/>
+                Yes
+                </label>
+               
+                <label for="lyricsNo">
+                <input type="radio" id="lyricsNo" name="lyricsIncluded" value="no" checked={formData.lyricsIncluded === "no"} onChange={handleChange}/>
+                No
+                </label>
+                <br />
+
+                {formData.lyricsIncluded === "yes" && (
+                <textarea 
+                type="text" 
+                id="lyricsText"
+                name="lyricsText" 
+                rows="5" cols="50" 
+                maxLength="400" 
+                placeholder="Type ideas, poems, lyrics, or say that you want me to write lyrics about soemthing!"
+                value={formData.lyricsText} 
+                onChange={handleChange}></textarea>
+                )}
+            </fieldset>
+
+            <label for="length">
+            Length: 
+            <input type="number" id="length" name="length" placeholder="desired length in seconds" value={formData.length} onChange={handleChange} ></input>
+            </label>
+
+            <fieldset>
+                <legend>For Someone?</legend>
+
+                    <label for="forSomeoneYes">
+                    <input type="radio" id="forSomeoneYes" name="forSomeone" value="yes" checked={formData.forSomeone === "yes"} onChange={handleChange}/>
+                    Yes
+                    </label>
+
+                    <label for="forSomeoneNo">
+                    <input type="radio" id="forSomeoneNo" name="forSomeone" value="no" checked={formData.forSomeone === "no"} onChange={handleChange}/>
+                    No
+                    </label>
+                    <br />
+
+                    {formData.forSomeone === "yes" && (
+                        <textarea 
+                        type="text"
+                        id="forSomeoneExplain"
+                        name="forSomeoneExplain"
+                        rows="5" cols="50" 
+                        maxLength="400" 
+                        placeholder="Who is this song for? this can be a person, group, or project. Please explain!"
+                        value={formData.forSomeoneExplain}
+                        onChange={handleChange}
+                        ></textarea>
+                    )}
+            </fieldset>
+            
+            <label for="emotions">
+                Emotions:
+                <br />
+              <textarea type="text" id="emotions" name="emotions" rows="6" cols="20" maxLength="500" placeholder="List some emotions you want to be conveyed" value={formData.emotions} onChange={handleChange}></textarea>
+            </label>
+            <br />
 
 
+            <label for="extraInfo">
+                    Additional Information:
+                    <br />
+                    <textarea type="text" id="extraInfo" name="extraInfo" rows="6" cols="20" maxLength="500" placeholder="Enter any other information you want me to know, or any specifc instruments/sounds you want included."
+                    value={formData.extraInfo} onChange={handleChange}></textarea>
+
+            </label>
+            
+            <fieldset>
+                <legend>How would you like the song delivered to you?</legend>
+
+                    {bounceOptions.map((option) => (
+                    <label key={option}>
+                        <input
+                        type="checkbox"
+                        name="bounce"
+                        value={option}
+                        checked={formData.bounce.includes(option)}
+                        onChange={handleChange}
+                        />
+                        {formatLabels[option] || option}
+                         </label>
+                    ))}
+            </fieldset>
             </form>
+
+            <div>
+                <h3>Your Song:</h3>
+            </div>
         </div>
     )   
 
