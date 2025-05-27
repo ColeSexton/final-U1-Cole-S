ddad7a240c1c40ef4d77aa8b30c649d78d14af9b
Cole Sexton <colesaudio88@gmail.com>
2025-05-27 09:52:23 -0500

    submit button works, and basic submission page

--- coles-webpage/src/components/Submission.jsx
+++ coles-webpage/src/components/Submission.jsx
@@ -1,19 +1,21 @@
+import SongForm from "./SongForm";
+
 const Submission = () =>{
     return(
-        <div>
+        <div className="submitPage">
 
             <div>
                 <h2>SUBMITTED SUCCESSFULLY</h2>
             </div>
 
-            <p>
+            <section>
                 Congrats, you submitted the Song Form!
                 I will reach out to you via the email you provided
                 to give you an invoice, and outline the next steps in the 
                 optional revision of your song!
                 If you have more questions, reach out to me via email
 
-            </p>
+            </section>
         </div>
     )
 
