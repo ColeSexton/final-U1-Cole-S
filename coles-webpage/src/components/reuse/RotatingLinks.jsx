c9fab0eb7eba41cbb35350f87326aef3543b6b51
Cole Sexton <colesaudio88@gmail.com>
2025-05-29 13:11:01 -0500

    header.css typo fixed

--- coles-webpage/src/components/reuse/RotatingLinks.jsx
+++ coles-webpage/src/components/reuse/RotatingLinks.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import './header.css';
+import './Header.css';
 
 const RotatingLinks = () =>{
     const links = [
